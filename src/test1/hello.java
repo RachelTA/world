7b710fdf4be26f6200635379c09f42133c4b0563
RachelTA <racheltang@tcl.com>
2017-11-07 15:06:37 +0800

    change

--- src/test1/hello.java
+++ src/test1/hello.java
@@ -5,7 +5,8 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("helloworld");
-
+		String s="111";
+		System.out.println(s);
 	}
 
 }
